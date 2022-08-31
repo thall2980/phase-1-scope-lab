1ddd0a3ac399f51e750da6ddcc8cfbfe5a97bf17
Tyler Hall <thall2980@gmail.com>
2022-08-30 20:08:33 -0400

    commit tree a0c42254f201d643245b240222bfeee5d0558f99

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'you'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'me'
+}
